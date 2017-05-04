b428e4509a4de7f6ee24082626b88a2d8d9de010
Miguel Fonseca <miguelcsf@gmail.com>
2017-05-04 15:57:40 +0100

    Keep SitesList's `selected` in sync with Redux
    - :-(
    
    - Aims to fix regressions found, notably around flows in `/domains`

--- client/state/lib/middleware.js
+++ client/state/lib/middleware.js
@@ -21,6 +21,10 @@ import cartStore from 'lib/cart/store';
 import { getSelectedSite, getSelectedSiteId } from 'state/ui/selectors';
 import { getCurrentUser } from 'state/current-user/selectors';
 
+// KILL IT WITH FIRE
+import sitesFactory from 'lib/sites-list';
+const sites = sitesFactory();
+
 const debug = debugFactory( 'calypso:state:middleware' );
 
 /**
@@ -45,6 +49,12 @@ if ( desktopEnabled ) {
  * providing an alternative to `sites.once()`.
  */
 let sitesListeners = [];
+
+const updateSelectedSiteForSitesList = ( dispatch, action, getState ) => {
+	const state = getState();
+	const selectedSiteId = getSelectedSiteId( state );
+	sites.select( selectedSiteId );
+};
 
 /**
  * Sets the selectedSite and siteCount for lib/analytics. This is used to
@@ -134,6 +144,7 @@ const handler = ( dispatch, action, getState ) => {
 		case SITES_UPDATE:
 			// Wait a tick for the reducer to update the state tree
 			setTimeout( () => {
+				updateSelectedSiteForSitesList( dispatch, action, getState );
 				updateSelectedSiteForCart( dispatch, action, getState );
 				if ( action.type === SITES_RECEIVE ) {
 					fireChangeListeners();
